7c402b05f7ae77176f609d546a537212b7d7802d
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-11-14 09:41:40 -0500

    chore: clean cache, log next version in build, fix videopage

--- web/app/video/page.tsx
+++ web/app/video/page.tsx
@@ -1,11 +1,16 @@
 import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 import { authOptions } from '@/utils/authOptions';
 import VideoPlayer from '@/components/VideoPlayer';
 
-export default function VideoPage() {
+export default async function VideoPage() {
   // check for valid session
-  const session = getServerSession(authOptions);
-  console.log(session);
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    // Redirect to login page if no session is found
+    redirect('/login');
+  }
 
   return (
     <div className="flex h-screen items-center justify-center bg-gray-100 text-black">
