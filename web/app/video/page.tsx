578011d53e039adcfe0643fe4acc2006a78a475e
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-11-14 10:09:16 -0500

    last try: adding strict mode to the next config
    This is making me a sad panda

--- web/app/video/page.tsx
+++ web/app/video/page.tsx
@@ -1,16 +1,12 @@
 import { getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
 import { authOptions } from '@/utils/authOptions';
 import VideoPlayer from '@/components/VideoPlayer';
 
 export default async function VideoPage() {
   // check for valid session
   const session = await getServerSession(authOptions);
-
-  if (!session) {
-    // Redirect to login page if no session is found
-    redirect('/login');
-  }
+  // instantiate session
+  console.log(session);
 
   return (
     <div className="flex h-screen items-center justify-center bg-gray-100 text-black">
